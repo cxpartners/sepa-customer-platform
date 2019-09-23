68f0c9bac3cc8e3a3386018f2eaef929664a20ad
Dave Martin <dave.martin@cxpartners.co.uk>
2019-09-23 14:34:02 +0100

    added embryonic index page

--- app/App.jsx
+++ app/App.jsx
@@ -1,7 +1,7 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Example from './pages/Example';
+import Index from './pages/Index';
 import Check from './pages/Check';
 import RadioForm from './pages/RadioForm';
 import TextForm from './pages/TextForm';
@@ -9,7 +9,7 @@ import TextForm from './pages/TextForm';
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Example} />
+      <Route exact path="/" component={Index} />
       <Route path="/radioform" component={RadioForm} />
       <Route path="/textform" component={TextForm} />
       <Route path="/check" component={Check} />
