bddef626b8302596c92c9dd44e5cb466d5dd5213
Dave Martin <1011822+justan0therdave@users.noreply.github.com>
2019-10-02 06:55:15 +0100

    add react-loadable (#35)

--- app/App.jsx
+++ app/App.jsx
@@ -1,54 +1,296 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop/component';
-import Index from './pages/Index';
-import UserTypes from './pages/UserTypes';
-import ArrivesAtStartPage from './pages/ArrivesAtStartPage';
-import StartPage from './pages/StartPage';
-import SepaHomePage from './pages/SepaHomePage';
-import SignUpSwitchContext from './pages/SignUpSwitchContext';
-import AccountPageSwitchContext from './pages/AccountPageSwitchContext';
-import Check from './pages/Check';
-import RadioForm from './pages/RadioForm';
-import SignUp from './pages/SignUp';
-import SignUpConfirmation from './pages/SignUpConfirmation';
-import SignUpTimePasses from './pages/SignUpTimePasses';
-import ReturnsToAddVariation from './pages/ReturnsToAddVariation';
-import SignUpEmailNotification from './pages/SignUpEmailNotification';
-import CreateAccountChoosePassword from './pages/CreateAccountChoosePassword';
-import CreateAccountAddCompanyDetails from './pages/CreateAccountAddCompanyDetails';
-import CreateAccountYourDetails from './pages/CreateAccountYourDetails';
-import CreateAccountConfirmation from './pages/CreateAccountConfirmation';
-import Permits from './pages/Permits';
-import PermitsWithNotifications from './pages/PermitsWithNotifications';
-import PermitPage from './pages/PermitPage';
-import PermitPageClas from './pages/PermitPageClas';
-import PreAppFormEnterCardDetails from './pages/PreAppFormEnterCardDetails';
-import PreAppFormConfirmation from './pages/PreAppFormConfirmation';
-import ReceivesPreAppFormEmail from './pages/ReceivesPreAppFormEmail';
-import PreAppFormStart from './pages/PreAppFormStart';
-import PreAppFormEmailNotification from './pages/PreAppFormEmailNotification';
-import PreAppFormLink from './pages/PreAppFormLink';
-import ReceivesPreAppReviewEmail from './pages/ReceivesPreAppReviewEmail';
-import PreAppReviewEmailNotification from './pages/PreAppReviewEmailNotification';
-import PreAppReviewLink from './pages/PreAppReviewLink';
-import PreAppReview from './pages/PreAppReview';
-import PreAppScreening from './pages/PreAppScreening';
-import ArrivesAtManagementDashboard from './pages/ArrivesAtManagementDashboard';
-import ManagementDashboard from './pages/ManagementDashboard';
-import PermitsTimePasses from './pages/PermitsTimePasses';
-import PreAppFormContactDetails from './pages/PreAppFormContactDetails';
-import PreAppFormSiteName from './pages/PreAppFormSiteName';
-import PreAppFormWaterBodyName from './pages/PreAppFormWaterBodyName';
-import PreAppFormEastingNorthing from './pages/PreAppFormEastingsNorthings';
-import PreAppFormSpecies from './pages/PreAppFormSpecies';
-import PreAppFormWeight from './pages/PreAppFormWeight';
-import PreAppFormFeedingRate from './pages/PreAppFormFeedingRate';
-import PreAppFormFeedMedicines from './pages/PreAppFormFeedMedicines';
-import PreAppFormBathMedicines from './pages/PreAppFormBathMedicines';
-import PreAppFormAdditional from './pages/PreAppFormAdditional';
-import PreAppFormCheck from './pages/PreAppFormCheck';
+import Loadable from 'react-loadable';
+
+const ScrollToTop = Loadable({
+  loader: () => import('./components/ScrollToTop/component'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const Index = Loadable({
+  loader: () => import('./pages/Index'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const UserTypes = Loadable({
+  loader: () => import('./pages/UserTypes'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const ArrivesAtStartPage = Loadable({
+  loader: () => import('./pages/ArrivesAtStartPage'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const StartPage = Loadable({
+  loader: () => import('./pages/StartPage'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const SepaHomePage = Loadable({
+  loader: () => import('./pages/SepaHomePage'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const SignUpSwitchContext = Loadable({
+  loader: () => import('./pages/SignUpSwitchContext'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const AccountPageSwitchContext = Loadable({
+  loader: () => import('./pages/AccountPageSwitchContext'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const Check = Loadable({
+  loader: () => import('./pages/Check'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const RadioForm = Loadable({
+  loader: () => import('./pages/RadioForm'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const SignUp = Loadable({
+  loader: () => import('./pages/SignUp'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const SignUpConfirmation = Loadable({
+  loader: () => import('./pages/SignUpConfirmation'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const SignUpTimePasses = Loadable({
+  loader: () => import('./pages/SignUpTimePasses'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const ReturnsToAddVariation = Loadable({
+  loader: () => import('./pages/ReturnsToAddVariation'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const SignUpEmailNotification = Loadable({
+  loader: () => import('./pages/SignUpEmailNotification'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const CreateAccountChoosePassword = Loadable({
+  loader: () => import('./pages/CreateAccountChoosePassword'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const CreateAccountAddCompanyDetails = Loadable({
+  loader: () => import('./pages/CreateAccountAddCompanyDetails'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const CreateAccountYourDetails = Loadable({
+  loader: () => import('./pages/CreateAccountYourDetails'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const CreateAccountConfirmation = Loadable({
+  loader: () => import('./pages/CreateAccountConfirmation'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const Permits = Loadable({
+  loader: () => import('./pages/Permits'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PermitsWithNotifications = Loadable({
+  loader: () => import('./pages/PermitsWithNotifications'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PermitPage = Loadable({
+  loader: () => import('./pages/PermitPage'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PermitPageClas = Loadable({
+  loader: () => import('./pages/PermitPageClas'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormEnterCardDetails = Loadable({
+  loader: () => import('./pages/PreAppFormEnterCardDetails'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormConfirmation = Loadable({
+  loader: () => import('./pages/PreAppFormConfirmation'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const ReceivesPreAppFormEmail = Loadable({
+  loader: () => import('./pages/ReceivesPreAppFormEmail'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormStart = Loadable({
+  loader: () => import('./pages/PreAppFormStart'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormEmailNotification = Loadable({
+  loader: () => import('./pages/PreAppFormEmailNotification'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormLink = Loadable({
+  loader: () => import('./pages/PreAppFormLink'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const ReceivesPreAppReviewEmail = Loadable({
+  loader: () => import('./pages/ReceivesPreAppReviewEmail'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppReviewEmailNotification = Loadable({
+  loader: () => import('./pages/PreAppReviewEmailNotification'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppReviewLink = Loadable({
+  loader: () => import('./pages/PreAppReviewLink'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppReview = Loadable({
+  loader: () => import('./pages/PreAppReview'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppScreening = Loadable({
+  loader: () => import('./pages/PreAppScreening'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const ArrivesAtManagementDashboard = Loadable({
+  loader: () => import('./pages/ArrivesAtManagementDashboard'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const ManagementDashboard = Loadable({
+  loader: () => import('./pages/ManagementDashboard'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PermitsTimePasses = Loadable({
+  loader: () => import('./pages/PermitsTimePasses'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormContactDetails = Loadable({
+  loader: () => import('./pages/PreAppFormContactDetails'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormSiteName = Loadable({
+  loader: () => import('./pages/PreAppFormSiteName'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormWaterBodyName = Loadable({
+  loader: () => import('./pages/PreAppFormWaterBodyName'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormEastingNorthing = Loadable({
+  loader: () => import('./pages/PreAppFormEastingsNorthings'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormSpecies = Loadable({
+  loader: () => import('./pages/PreAppFormSpecies'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormWeight = Loadable({
+  loader: () => import('./pages/PreAppFormWeight'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormFeedingRate = Loadable({
+  loader: () => import('./pages/PreAppFormFeedingRate'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormFeedMedicines = Loadable({
+  loader: () => import('./pages/PreAppFormFeedMedicines'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormBathMedicines = Loadable({
+  loader: () => import('./pages/PreAppFormBathMedicines'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormAdditional = Loadable({
+  loader: () => import('./pages/PreAppFormAdditional'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+const PreAppFormCheck = Loadable({
+  loader: () => import('./pages/PreAppFormCheck'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
 
 const App = () => (
   <Router>
