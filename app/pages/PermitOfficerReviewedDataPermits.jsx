b2e5003bf179d88831c3deca5132855675910c0d
Ross Penlington <ross.penlington@cxpartners.co.uk>
2019-10-16 14:13:40 +0100

    Update content on Your permit page

--- app/pages/PermitOfficerReviewedDataPermits.jsx
+++ app/pages/PermitOfficerReviewedDataPermits.jsx
@@ -43,31 +43,13 @@ const Permit = () => (
         </Row>
         <Row>
           <Column columnWidth="full">
-            <PermitList caption="In progress (20)" dateColumn="Last updated">
-              <PermitListRow key="1" listKey="CAR/L/4336591" status="pre-application (2)" isActive href="/" siteName="Kimelford" waterBody="Loch Tarbot Gallaway" date="17/02/2019" />
-              <PermitListRow key="2" listKey="CAR/L/4336592" status="pre-application (1)" isActive href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="3" listKey="CAR/L/4336592" status="application" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="4" listKey="CAR/L/4336592" status="application" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="5" listKey="CAR/L/4336592" status="application" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="6" listKey="CAR/L/4336592" status="application" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="7" listKey="CAR/L/4336592" status="application" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="8" listKey="CAR/L/4336592" status="assessment" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="9" listKey="CAR/L/4336592" status="assessment" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="10" listKey="CAR/L/4336592" status="assessment" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="11" listKey="CAR/L/4336592" status="assessment" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="12" listKey="CAR/L/4336592" status="consultation" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="13" listKey="CAR/L/4336592" status="consultation" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="14" listKey="CAR/L/4336592" status="consultation" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="15" listKey="CAR/L/4336592" status="consultation" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="16" listKey="CAR/L/4336592" status="permit" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="17" listKey="CAR/L/4336592" status="permit" href="/" siteName="LKimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="18" listKey="CAR/L/4336592" status="permit" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="19" listKey="CAR/L/4336592" status="monitoring" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
-              <PermitListRow key="20" listKey="CAR/L/4336592" status="monitoring" href="/" siteName="Kimelford" waterBody="Loch Katrine" date="17/02/2019" />
+            <PermitList caption="In progress (4)" dateColumn="Last updated">
+              <PermitListRow key="1" listKey="CAR/L/4336581" status="pre-application (2)" isActive href="/" siteName="Kimelford" waterBody="Lauren McKenna" date="17/02/2019" />
+              <PermitListRow key="2" listKey="CAR/L/4336580" status="pre-application (1)" isActive href="/" siteName="Ardfern" waterBody="Lauren McKenna" date="15/02/2019" />
+              <PermitListRow key="3" listKey="CAR/L/4336579" status="application" href="/" siteName="Kimelford" waterBody="Lauren McKenna" date="15/01/2019" />
+              <PermitListRow key="4" listKey="CAR/L/4336578" status="application" href="/" siteName="Kimelford" waterBody="Lauren McKenna" date="12/01/2019" />
             </PermitList>
-            <Details modifier="govuk-!-font-weight-bold" title="Awarded permits (207)">&nbsp;</Details>
-            <Details modifier="govuk-!-font-weight-bold" title="Surrendered permits (22)">&nbsp;</Details>
-
+            <Details modifier="govuk-!-font-weight-bold" title="Awarded permits (21)">&nbsp;</Details>
           </Column>
         </Row>
       </Main>
